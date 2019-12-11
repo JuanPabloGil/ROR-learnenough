5567c82434292b84d16bf7c253cca6828afdb5b4
Juan Pablo <jpablomgil@gmail.com>
2019-12-11 12:28:43 -0600

    authenticated? should return false for a user with nil digest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
