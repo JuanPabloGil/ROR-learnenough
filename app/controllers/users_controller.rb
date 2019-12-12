7eeeec89c07d9a120ac03996d051fcb0e08ac789
Juan Pablo <jpablomgil@gmail.com>
2019-12-12 09:00:56 -0600

    Showing all the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
 
   def new
     @user = User.new
   end
 
+
   def show
     @user = User.find(params[:id])
   end
+
 
   def create
     @user = User.new(user_params)
@@ -22,9 +29,11 @@ class UsersController < ApplicationController
     end
   end
 
+
   def edit
     @user = User.find(params[:id])
   end
+
 
   def update
     @user = User.find(params[:id])
@@ -35,6 +44,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
 
   private
 
